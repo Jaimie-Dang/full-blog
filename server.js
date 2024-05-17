52cfa5a4c68fa3b580faf2c7c6e56abc62806b32
Tuyen Dang <tuyendang878@gmail.com>
2024-05-18 02:39:25 +0700

    Cung cấp các phần tử, cấu trúc cho editor.html + định dạng edit.css

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ let initial_path = path.join(__dirname, "public");
 const app = express();
 app.use(express.static(initial_path));
 
+// route tới trang chủ
 app.get("/", (req, res) => {
   res.sendFile(path.join(initial_path, "home.html"));
+});
+
+// route tới trang editor
+app.get("/editor", (req, res) => {
+  res.sendFile(path.join(initial_path, "editor.html"));
 });
 
 // start server by "npm start"
